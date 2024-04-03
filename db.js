a023435c0faf221e2ccd295c44ff9615f4fdc360
jagritbhaker45 <bhaker.jagrit20@gmail.com>
2024-04-03 23:37:38 +0530

    fourth commit

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose=require("mongoose");
+require('dotenv').config();
 
 // Define the mongodb connection url
-const mongoURL='mongodb://localhost:27017/hotels'
+//const mongoURL='mongodb://localhost:27017/hotels'
+const mongoURL=process.env.MONGODB_URL;
 
 //set up mongodb connection
 mongoose.connect(mongoURL,{
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express=require('express');
 const app=express();
 const db=require('./db');
+require('dotenv').config();
 const bodyparser=require('body-parser');
 app.use(bodyparser.json());
-
+const PORT=process.env.PORT || 3000;
 
 
 app.get("/",function(req,res){
@@ -19,6 +20,7 @@ const menuitemroutes=require('./routes/menuitemroutes');
 // use the routers
 app.use('/person',personroutes);
 app.use('/menuitem',menuitemroutes);
+
 
 app.listen(3000,()=>{
   console.log("server is running at port no 3000");
